0cb264e22a7b1ef988ce30d6fc84fb71949dc7d3
MikeProduce <Gomez_miguelomar@yahoo.com>
2022-09-06 23:11:42 -0500

    yea

--- index.js
+++ index.js
@@ -127,3 +127,13 @@ cardNum.addEventListener("input", function (e) {
     .replace(/(.{4})/g, "$1 ")
     .trim();
 });
+
+window.onkeypress = function (e) {
+  if (e.which == 13) {
+    e.preventDefault();
+    var nextInput = inputs.get(inputs.index(document.activeElement) + 1);
+    if (nextInput) {
+      nextInput.focus();
+    }
+  }
+};
